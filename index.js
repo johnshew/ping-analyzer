9e707a6487cf8b1620bf6279860e78989dfced45
John Shewchuk <johnshew@users.noreply.github.com>
2020-05-08 11:33:41 -0700

    Works on Windows, bigger graph, tighter metrics

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const chalk = require('chalk');
 const dns = require('dns');
 const asciichart = require('asciichart')
 
+
+const GRAPH_MAX = 180;
+const GRAPH_TICK = 0;
+const MAX_GRAPH_VALUE = 300;  // Maximum y-height represents max usable ping latency
+
 const GRAPH_CONFIG = {
-  height: 10,
-  width: 80,
+  height: 40,
+  width: GRAPH_MAX,
   offset: 2,
   padding: '        ',
 };
 
-const GRAPH_MAX = 60;
-const GRAPH_TICK = 5;
-const MAX_GRAPH_VALUE = 300;  // Maximum y-height represents max usable ping latency
-
 // Minimum latency to consider unusable
 const TIMEOUT_LATENCY_MIN = 101;
 
-const RE_PING_RESPONSE = /([\d]*) bytes from ([\d\.]*): icmp_seq=([\d]*) ttl=([\d]*) time=([\d\.]*) ms/;
-const RE_TIMEOUT_RESPONSE = /Request timeout for icmp_seq ([\d]*)/
+const RE_PING_RESPONSE_UNIX = /([\d]*) bytes from ([\d\.]*): icmp_seq=([\d]*) ttl=([\d]*) time=([\d\.]*) ms/;
+const RE_TIMEOUT_RESPONSE_UNIX = /Request timeout for icmp_seq ([\d]*)/
+
+const RE_PING_RESPONSE_WIN32 = /.* from ([\d\.]*): bytes=([\d]*) time=([\d\.]*).?ms TTL=([\d]*)/;
+const RE_TIMEOUT_RESPONSE_WIN32 = /.*[(timeout)|(failure)|(timed out)].*/
+
+// Should check OS and do this automatically
+const RE_PING_RESPONSE = RE_PING_RESPONSE_WIN32;
+const RE_TIMEOUT_RESPONSE = RE_TIMEOUT_RESPONSE_WIN32;
 
 const ERROR_HIGHLIGHT = chalk.bgRed.white;
 const SUCCESS_HIGHLIGHT = chalk.bgGreen.white;
@@ -78,7 +86,7 @@ setInterval(() => {
   })
 }, 1000);
 
-rl.on('line', function(line) {
+rl.on('line', function (line) {
   const responseMatch = RE_PING_RESPONSE.exec(line);
   const timeoutMatch = RE_TIMEOUT_RESPONSE.exec(line);
 
@@ -87,11 +95,10 @@ rl.on('line', function(line) {
   let latencyLevel = 'ok';
 
   if (responseMatch) {
-    const bytes = parseInt(responseMatch[1], 10);
-    const ip = responseMatch[2];
-    const icmp = parseInt(responseMatch[3], 10);
+    const ip = responseMatch[1];
+    const bytes = parseInt(responseMatch[2], 10);
+    const time = parseFloat(responseMatch[3]);
     const ttl = parseInt(responseMatch[4], 10);
-    const time = parseFloat(responseMatch[5]);
 
     responseNum++;
     latencySum += time;
@@ -126,7 +133,7 @@ rl.on('line', function(line) {
     isOnline = false;
 
     // Show high number on graph to represent timeout
-    push_to_graph(10000);
+    push_to_graph(MAX_GRAPH_VALUE);
   } else {
     return;
   }
@@ -143,21 +150,19 @@ rl.on('line', function(line) {
 
   console.clear();
 
-  console.log(chalk.underline.bold('CURRENT STATE'));
+  let jitter = (latencyGraph.reduce((p, c, i, a) => (i === 0) ? 0 : (p + Math.abs(a[i] - a[i - 1])), 0) / responseNum).toFixed(1);
+  console.log();
+  console.log(`${(latencySum / responseNum).toFixed(1)} ms (${Math.min(...latencyGraph)} to ${Math.max(...latencyGraph)} ms) ${jitter} ms`);
+  console.log();
 
-  if (isOnline) {
-    console.log(SUCCESS_HIGHLIGHT('Online'));
-  } else {
-    console.log(ERROR_HIGHLIGHT('Offline'));
+  if (latencyGraph.length > 2) {
+    console.log(asciichart.plot(latencyGraph, GRAPH_CONFIG));
   }
 
-  if (dnsLookupStatus) {
-    console.log(SUCCESS_NORMAL(`DNS: true`));
-  } else {
-    console.log(ERROR_NORMAL(`DNS: false`));
-  }
+  console.log();
+  console.log(`${(isOnline) ? SUCCESS_HIGHLIGHT('Online') : ERROR_HIGHLIGHT('Offline')} ${((numOnline / (numOnline + numOffline)) * 100).toFixed(1)}% (${numOnline}s:${numOffline}s) ${numTimeouts} timed out. ${dnsLookupStatus ? 'Using DNS' : ''}`);
 
-  switch(latencyLevel) {
+  switch ('off' || latencyLevel) {
     case 'ok':
       console.log(SUCCESS_NORMAL(`Latency: ${lastLatency}`));
       break;
@@ -167,19 +172,8 @@ rl.on('line', function(line) {
     case 'error':
       console.log(ERROR_NORMAL(`Latency: ${lastLatency}`));
       break;
+    default:
+      break;
   }
 
-  console.log();
-  console.log(chalk.underline.bold('TRIP SUMMARY'));
-  console.log(`Average latency: ${(latencySum / responseNum).toPrecision(5)}ms`);
-  console.log(`Max Time Online: ${maxTimeOnline}s`);
-  console.log('Timeouts:', numTimeouts);
-  console.log(`Offline: ${numOffline}s`);
-  console.log(`Online: ${numOnline}s`);
-  console.log('Online Percentage:', `${((numOnline / (numOnline + numOffline)) * 100).toPrecision(4)}%`);
-  console.log();
-
-  if (latencyGraph.length > 2) {
-    console.log(asciichart.plot(latencyGraph, GRAPH_CONFIG));
-  }
 });
